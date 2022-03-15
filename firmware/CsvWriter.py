fd2f2985edd79e06afb5710dca88232b0c23abaa
Callum Snowden <10308126+callumsnowden@users.noreply.github.com>
2022-03-15 16:33:48 +0000

    Fixes and improvements
    - Fix #13 so that PrometheusWriter now accepts between HTTP 200 and 299.
    - Add support for #12 across aq application.
    - Fix #11 to make minimum Prometheus write interval 120s
    - Fix #10, proper support for RFC1738 URLs
    - Add support for additional Prometheus labels, closes #9
    - Code updated to support new logger options in DesignSpark.ESDK
    - Added example debugging webpage to pull additional values from websocket
    - Implemented error checking on getAppCommitHash() function to return an empty string if a Git error occurs

--- firmware/CsvWriter.py
+++ firmware/CsvWriter.py
@@ -11,11 +11,13 @@ import copy
 from datetime import datetime
 
 class CsvWriter:
-    def __init__(self, friendlyName='', debug=False, hwid=0):
-        self.logger = AppLogger.getLogger(__name__, debug)
+    def __init__(self, friendlyName='', debug=False, hwid=0, loggingLevel='full'):
+        self.logger = AppLogger.getLogger(__name__, debug, loggingLevel)
         self.hardwareId = hwid
         self.friendlyName = friendlyName
-        self.csvFilename = "/aq/data/{fn}_{hwid}_{ts}.csv".format(fn=self.friendlyName, hwid=self.hardwareId, ts=datetime.utcnow().strftime("%Y_%m_%d-%H_%M_%S"))
+        self.csvFilename = "/aq/data/{fn}_{hwid}_{ts}.csv".format(fn=self.friendlyName, \
+            hwid=self.hardwareId, \
+            ts=datetime.utcnow().strftime("%Y_%m_%d-%H_%M_%S"))
 
         # Should more sensors be added, this should be updated to reflect available values
         self.csvColumns = ['timestamp', 'temperature', 'humidity', 'vocIndex', 'co2', 'pm1.0', 'pm2.5', 'pm4.0', 'pm10']
